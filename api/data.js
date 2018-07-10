ed963b100dea70a2652c9c8c7407555fdcba56e2
phpswain <15377566335@163.com>
2018-07-10 14:23:10 +0800

    第一次上传‘ git commit -m 第一次上传‘

---
+++ api/data.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+export var getNewsList = ()=>{
+  //把结果return出去，基于promise,结果会放在axios中的then中
+  return axios
+    .get('../../static/newslist.json')
+    .then(res=>res.data);
+}
+
+export var getNewsList1 = ()=>{
+  //把结果return出去，基于promise,结果会放在axios中的then中
+  return axios
+    .get('../../static/newslist1.json')
+    .then(res=>res.data);
+}
+export var getCaiList = ()=>{
+  // return axios
+  //   .get('../../static/cai.json')
+  //   .then(res=>res.data);
+}
